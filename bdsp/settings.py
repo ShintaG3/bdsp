976119b2ea7e4b77697613b69252817358cf2d04
ShintaG3 <snishino2013@gmail.com>
2019-08-29 22:58:28 +0300

    responsive table detail page

--- bdsp/settings.py
+++ bdsp/settings.py
@@ -113,7 +113,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    # '/var/www/static/',
 ]
 
 LOGIN_REDIRECT_URL = '/'
