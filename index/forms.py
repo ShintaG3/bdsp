d62430b49d7f795c6c9d551b1734152630db1c54
ShintaG3 <snishino2013@gmail.com>
2019-09-26 10:17:53 +0900

    forms.py and updateview added css

--- index/forms.py
+++ index/forms.py
@@ -1,22 +1,58 @@
 from django.forms import ModelForm
+from django import forms
 from index.models import OrgBaseInfo, Service, Experience, Case
+
 
 class ExperienceForm(ModelForm):
     class Meta:
         model = Experience
         exclude = ['OrgName']
 
+
 class ServiceForm(ModelForm):
     class Meta:
         model = Service
         fields = '__all__'
+
 
 class CaseForm(ModelForm):
     class Meta:
         model = Case
         fields = '__all__'
 
+
 class OrgBaseInfoForm(ModelForm):
     class Meta:
         model = OrgBaseInfo
         fields = '__all__'
+
+    Name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Name"
+            }
+        )
+    )
+
+    # models.CharField(
+    #     max_length=100, help_text="Enter the name of Organisation(eg. Metal Industries)")
+    # Address = models.CharField(
+    #     max_length=200, help_text="Enter the adress of the Organisation")
+    # RegistrationDate = models.DateField()
+    # Industry = models.ManyToManyField('Industry')
+    # ServiceCategory = models.ManyToManyField('ServiceCategory')
+    # PR = models.CharField(max_length=300)
+    # Url = models.URLField(max_length=50)
+    # Affiliation = models.CharField(max_length=50)
+    # ContactPerson = models.CharField(max_length=50)
+    # Email = models.EmailField()
+    # Telephone = models.CharField(
+    #     max_length=12, help_text="Enter the telephone number for the organisation")
+
+    # Region = models.CharField(
+    #     max_length=30,
+    #     choices=Region_data,
+    #     blank=True,
+    #     default='ada',
+    #     help_text='Region of the company')
--- index/views.py
+++ index/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Industry, ServiceCategory, Region_data, OrgBaseInfo, Service, Case, Experience
@@ -8,8 +9,10 @@ from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import *
 
-def index (request):
+
+def index(request):
     regiondata = []
     for region in Region_data:
         regiondata.append(region[0])
@@ -20,18 +23,19 @@ def index (request):
     }
     return render(request, 'index/index.html', context=context)
 
-def list (request):
+
+def list(request):
     if request.method == 'POST':
         regions = request.POST.getlist('region')
         industries = request.POST.getlist('industry')
         services = request.POST.getlist('service')
     # Send empty if nothing selected
-        if len(regions)==0 and len(industries)==0 and len(services)==0:
+        if len(regions) == 0 and len(industries) == 0 and len(services) == 0:
             context = {
             }
             return render(request, 'index/list.html', context=context)
 
-    #chaining the  input data for querying
+    # chaining the  input data for querying
         if len(regions) == 0:
             for region in Region_data:
                 regions.append(region[0])
@@ -47,7 +51,8 @@ def list (request):
                 services.append(service.Name)
     # Query Codes here
         query_result = OrgBaseInfo.objects.filter(
-        Q(Region__in=regions) & Q(Industry__Name__in=industries) & Q(ServiceCategory__Name__in=services)
+            Q(Region__in=regions) & Q(Industry__Name__in=industries) & Q(
+                ServiceCategory__Name__in=services)
         ).distinct().order_by('Region')
         context = {
             'query_result': query_result
@@ -56,39 +61,41 @@ def list (request):
     query_result = OrgBaseInfo.objects.all()
     context = {
         'query_result': query_result
-        }
+    }
     return render(request, 'index/list.html', context=context)
 
-def details (request, id):
+
+def details(request, id):
     org = get_object_or_404(OrgBaseInfo, pk=id)
     region = org.Region
     services = Service.objects.filter(OrgName=org)
     cases = Case.objects.filter(OrgName=org)
     experiences = Experience.objects.filter(OrgName=org)
-    context={
-    'org': org,
-    'services': services,
-    'cases': cases,
-    'experiences': experiences
+    context = {
+        'org': org,
+        'services': services,
+        'cases': cases,
+        'experiences': experiences
     }
     return render(request, 'index/details.html', context=context)
 
 
-
 def search(request):
     orgInfo = request.POST["orgInfo"]
-    orgs = OrgBaseInfo.objects.filter(Name__icontains=orgInfo).order_by('Region')
-    return render(request,'index/list.html',context={'query_result':orgs})
+    orgs = OrgBaseInfo.objects.filter(
+        Name__icontains=orgInfo).order_by('Region')
+    return render(request, 'index/list.html', context={'query_result': orgs})
 
-from django.urls import reverse_lazy
 
 class ServiceCreate(LoginRequiredMixin, CreateView):
     model = Service
     fields = '__all__'
 
+
 class ServiceUpdate(LoginRequiredMixin, UpdateView):
     model = Service
     fields = ['ServiceCategory', 'Service', 'Contents']
+
 
 @login_required
 def ServiceDelete(request, pk):
@@ -97,13 +104,16 @@ def ServiceDelete(request, pk):
     service.delete()
     return redirect('details', id=int(org.id))
 
+
 class CaseCreate(LoginRequiredMixin, CreateView):
     model = Case
     fields = '__all__'
 
+
 class CaseUpdate(LoginRequiredMixin, UpdateView):
     model = Case
     fields = ['ServiceCategory', 'Contents', 'Result']
+
 
 @login_required
 def CaseDelete(request, pk):
@@ -111,23 +121,32 @@ def CaseDelete(request, pk):
     org = OrgBaseInfo.objects.get(Name=case.OrgName)
     case.delete()
     return redirect('details', id=int(org.id))
-    
+
+
 class ExperienceCreate(LoginRequiredMixin, CreateView):
     model = Experience
     fields = '__all__'
 
+
 class ExperienceUpdate(LoginRequiredMixin, UpdateView):
     model = Experience
     fields = ['Large', 'Medium', 'SmallandMicro']
+
 
 class ExperienceDelete(LoginRequiredMixin, DeleteView):
     model = Experience
     success_url = reverse_lazy('index')
 
 # Registration of New Org:
+
+
 class OrgbaseInfoCreate(LoginRequiredMixin, CreateView):
     model = OrgBaseInfo
-    fields = '__all__'
+    form_class = OrgBaseInfoForm
+    # success_url = reverse_lazy('index')
+
+    # fields = '__all__'
+
 
 class OrgBaseInfoUpdate(LoginRequiredMixin, UpdateView):
     model = OrgBaseInfo
