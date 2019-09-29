907c82584fede3366913c49ea9991bb49ba61d4c
vikas <vikassrinitb@gmail.com>
2019-09-29 15:47:23 +0530

    some codes for forms added

--- index/forms.py
+++ index/forms.py
@@ -1,7 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from index.models import OrgBaseInfo, Service, Experience, Case
-
+from index.models import OrgBaseInfo, Service, Experience, Case, Industry
 
 class ExperienceForm(ModelForm):
     class Meta:
@@ -22,19 +21,73 @@ class CaseForm(ModelForm):
 
 
 class OrgBaseInfoForm(ModelForm):
+
     class Meta:
         model = OrgBaseInfo
         fields = '__all__'
-
     Name = forms.CharField(
         widget=forms.TextInput(
             attrs={
                 "class": "form-control",
-                "placeholder": "Name"
+                "placeholder": "Name of Organisation"
             }
         )
     )
-
+    Address = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Address Of Organisation"
+            }
+        )
+    )
+    RegistrationDate = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
+    PR = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "PR of Organisation"
+            }
+        )
+    )
+    Url = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Url of Organisation"
+            }
+        )
+    )
+    Affiliation = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Affiliation of Organisation"
+            }
+        )
+    )
+    ContactPerson = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Contact Person for the Organisation"
+            }
+        )
+    )
+    Email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Email for the Organisation"
+            }
+        )
+    )
     # models.CharField(
     #     max_length=100, help_text="Enter the name of Organisation(eg. Metal Industries)")
     # Address = models.CharField(
